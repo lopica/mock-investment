e6d1780935a5db14a2187fbe9a32b45fadaa95de
lopica <ducanhedison@gmail.com>
2025-02-15 15:27:14 +0700

    add image

--- constants.js
+++ constants.js
@@ -1,20 +1,26 @@
 export const DEFAULT_MONEY_JARS = [
     {
         name: "Emergency",
+        image: "0",
     },
     {
         name: "Safe Saving",
+        image: "25",
     },
     {
         name: "Daily",
+        image: "50",
     },
     {
         name: "Investment",
+        image: "75",
     },
     {
         name: "Education",
+        image: "100",
     },
     {
         name: "Entertain",
+        image: "100",
     },
 ]
